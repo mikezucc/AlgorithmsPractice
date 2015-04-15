5fb92f3d0dd3e7499b6c8e90b1846f161150b5b8
Michael Zuccarino <mikezuccarino@gmail.com>
2015-04-15 02:17:50 -0700

    with added test code for plaindrome code

--- String-Array-Matrix/Longest Palindrome/LongPalindrome1/LongPalindrome1/Source.cpp
+++ String-Array-Matrix/Longest Palindrome/LongPalindrome1/LongPalindrome1/Source.cpp
@@ -93,30 +93,74 @@ int main() {
 	string palinDomeEnd = "2345ljkhlj234h5l234jk5hlkjhljk3h45234lkjlongassstringforthatpalinstomeemotsnilaptahtrofgnirtsssagnol";
 	string palinDomeStart = "longassstringforthatpalinstomeemotsnilaptahtrofgnirtsssagnol2345ljkhlj234h5l234jk5hlkjhljk3h45234lkj";
 	string palinDomeMiddle = "2345ljkhlj2345dfg34h5l2longassstringforthatpalinstomeemotsnilaptahtrofgnirtsssagnol34jk5hlkj345dfgdfghljk3h45234lkj";
-	cout << "PALINSTART" << endl;
+	cout << "PALINSTART, with palindrome: " << "longassstringforthatpalinstomeemotsnilaptahtrofgnirtsssagnol" << endl;
+	cout << endl;
 	clock_t t1, t2, t3, t4;
-	
+	cout << "Palindrome at end" << endl;
 	t3 = clock();
 	string longestPalinOld;
-	for (int p = 0; p < 50; p++)
+	for (int p = 0; p < 100; p++)
 	{
-		longestPalinOld = findLongestPalindromeOptimized(palinDomeMiddle);
+		longestPalinOld = findLongestPalindromeOptimized(palinDomeEnd);
 		
 	}
 	t4 = clock();
 	float diff2((float)t4 - (float)t3);
-	cout << "opt: " << diff2 << endl;
-	cout << "longestPalinNonOpt: " << longestPalinOld << endl;
+	cout << "opt:    " << diff2 << endl;
 
 	t1 = clock();
-	for (int p = 0; p < 50; p++)
+	for (int p = 0; p < 100; p++)
 	{
-		string longestPalin = findLongestPalindrome(palinDomeMiddle);
+		string longestPalin = findLongestPalindrome(palinDomeEnd);
 		//cout << "longestPalin: " << longestPalin << endl;
 	}
 	t2 = clock();
 	float diff1((float)t2 - (float)t1);
 	cout << "nonopt: " << diff1 << endl;
+	cout << endl;
+
+	cout << "Palindrome at Beginning" << endl;
+	t3 = clock();
+	for (int p = 0; p < 100; p++)
+	{
+		longestPalinOld = findLongestPalindromeOptimized(palinDomeStart);
+
+	}
+	t4 = clock();
+	float diff3((float)t4 - (float)t3);
+	cout << "opt:    " << diff3 << endl;
+
+	t1 = clock();
+	for (int p = 0; p < 100; p++)
+	{
+		string longestPalin = findLongestPalindrome(palinDomeStart);
+		//cout << "longestPalin: " << longestPalin << endl;
+	}
+	t2 = clock();
+	float diff4((float)t2 - (float)t1);
+	cout << "nonopt: " << diff4 << endl;
+	cout << endl;
+
+	cout << "Palindrome at Middle" << endl;
+	t3 = clock();
+	for (int p = 0; p < 100; p++)
+	{
+		longestPalinOld = findLongestPalindromeOptimized(palinDomeMiddle);
+
+	}
+	t4 = clock();
+	float diff6((float)t4 - (float)t3);
+	cout << "opt:    " << diff6 << endl;
+
+	t1 = clock();
+	for (int p = 0; p < 100; p++)
+	{
+		string longestPalin = findLongestPalindrome(palinDomeMiddle);
+		//cout << "longestPalin: " << longestPalin << endl;
+	}
+	t2 = clock();
+	float diff5((float)t2 - (float)t1);
+	cout << "nonopt: " << diff5 << endl;
 
 	return 0;
 }
